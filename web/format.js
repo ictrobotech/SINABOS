785e16383dc51ccb1715981cd36a508b3670c7a5
ICT - ROBOTECH <vred0445@gmail.com>
2026-09-20 12:39:58 +0800

    Delete web directory

--- web/format.js
+++
@@ -1,6 +0,0 @@
-// Teks berpotensi formula dijadikan teks; nilai numerik asli tetap dapat dijumlahkan.
-export function csvCell(value) {
-  let s=String(value??'');
-  if (typeof value!=='number' && (/^[\s]*[=+\-@]/.test(s)||/^[\t\r\n]/.test(s))) s="'"+s;
-  return '"'+s.replace(/"/g,'""')+'"';
-}
